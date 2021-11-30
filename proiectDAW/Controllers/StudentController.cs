4442e74dabd380e6b6f89ae5b3a87cff00ed6230
claudia <ursu.claudia99@yahoo.com>
2021-11-30 13:11:10 +0200

    initial project structure with swagger

---
+++ proiectDAW/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using proiectDAW.Models;
+
+namespace proiectDAW.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public static List<Student> students = new List<Student>
+        {
+            new Student(1, "test", 12),
+            new Student(2, "test1", 13)
+        };
+
+        [HttpGet("getStudents")]
+        public List<Student> getStudents()
+        {
+            return students;
+        }
+    }
+}
